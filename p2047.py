fd58287d3403341210f89fcd8968f49cecfb991c
StephenYuan7 <2583242079@qq.com>
2022-01-27 22:51:23 +0800

    Update p2047.py

--- p2047.py
+++ p2047.py
@@ -1,27 +1,24 @@
 class Solution:
     def countValidWords(self, sentence: str) -> int:
-        s = sentence.split()
         r = 0
-        for word in s:
-            count_ = 0
-            t = 1
-            for index, i in enumerate(word):
-                if '0'<= i <='9':
-                    t = 0
-                    break
-                if i == '-':
-                    if count_ == 0:
-                        count_ = 1
-                        if index == 0 or index == len(word)-1 or word[index - 1] < 'a' or word[index - 1] > 'z' or word[
-                            index + 1] < 'a' or word[index + 1] > 'z':
-                            t = 0
-                            break
-                    else:
-                        t = 0
-                        break
-                if (i == '!' or i == '.' or i == ',') and index != len(word)-1:
-                    t = 0
-                    break
-            if t:
+        for word in sentence.split():
+            if self.valid(word):
                 r += 1
         return r
+
+    def valid(self, word):
+        count_ = 0
+        for index, i in enumerate(word):
+            if i.isdigit():
+                return False
+            if i == '-':
+                if count_ == 0:
+                    if index == 0 or index == len(word) - 1 or not word[index - 1].islower() \
+                            or not word[index + 1].islower():
+                        return False
+                    count_ = 1
+                else:
+                    return False
+            if (i == '!' or i == '.' or i == ',') and index != len(word) - 1:
+                return False
+        return True
