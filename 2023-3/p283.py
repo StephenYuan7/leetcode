8f74cba455d5cd0908b0fbb4a21d4c7edfc31d72
StephenYuan7 <2583242079@qq.com>
2023-03-11 18:33:07 +0800

    p283

---
+++ 2023-3/p283.py
@@ -0,0 +1,17 @@
+'''
+依次数是第几个非零元素就行
+'''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in nums:
+            if i != 0:
+                nums[j] = i
+                j += 1
+        while j < len(nums):
+            nums[j] = 0
+            j += 1
+
