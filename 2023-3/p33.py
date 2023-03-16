10f0d9644e3ee08c8693b599a768bb001802c45d
StephenYuan7 <2583242079@qq.com>
2023-03-16 10:46:46 +0800

    p33改进

--- 2023-3/p33.py
+++ 2023-3/p33.py
@@ -42,6 +42,27 @@ class Solution:
                 return mid + x
         return -1
 
+    def mysearch(self, nums, target: int):
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[left] < nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid + 1] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        if nums[left] == target:
+            return left
+        return -1
 
-print(Solution().search([4,5,1,2,3], 1))
+
+print(Solution().mysearch([4,5,6,7,0,1,2], 3))
 
