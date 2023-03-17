479765d3ed09248d927424042eb6efcd6f12941f
StephenYuan7 <2583242079@qq.com>
2023-03-17 10:19:56 +0800

    p31

--- 2023-3/offer29.py
+++ 2023-3/offer29.py
@@ -30,7 +30,3 @@ class Solution:
             left = left + 1
         return r
 
-
-print(Solution().spiralOrder([[1,2,3,4],
-                              [5,6,7,8],
-                              [9,10,11,12]]))
