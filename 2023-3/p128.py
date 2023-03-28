62bf37b21c6be1405edb0afb09c12b4fbc84e9fd
StephenYuan7 <2583242079@qq.com>
2023-03-28 15:36:08 +0800

    p128

--- 2023-3/p128.py
+++ 2023-3/p128.py
@@ -3,17 +3,18 @@ class Solution:
         x = {i: 1 for i in nums}
         r = 0
         for i in x:
-            a = i + 1
-            b = i - 1
-            while a in x and x[a] == 1:
-                x[a] = 0
-                a += 1
+            if x[i] == 1:
+                a = i + 1
+                b = i - 1
+                while a in x and x[a] == 1:
+                    x[a] = 0
+                    a += 1
 
-            while b in x and x[b] == 1:
-                x[b] = 0
-                b -= 1
+                while b in x and x[b] == 1:
+                    x[b] = 0
+                    b -= 1
 
-            r = max(a - b - 1, r)
+                r = max(a - b - 1, r)
         return r
 
 
