afbe75c72c78dae8cc5eaa8746d171d8e33d1a6a
StephenYuan7 <2583242079@qq.com>
2023-03-28 15:33:57 +0800

    p128

---
+++ 2023-3/p128.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums) -> int:
+        x = {i: 1 for i in nums}
+        r = 0
+        for i in x:
+            a = i + 1
+            b = i - 1
+            while a in x and x[a] == 1:
+                x[a] = 0
+                a += 1
+
+            while b in x and x[b] == 1:
+                x[b] = 0
+                b -= 1
+
+            r = max(a - b - 1, r)
+        return r
+
+
+print(Solution().longestConsecutive([100,4,200,1,3,2]))
