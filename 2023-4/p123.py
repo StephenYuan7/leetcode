7f5852b8692a5565ef27b6582b957afb44c7f118
StephenYuan7 <2583242079@qq.com>
2023-04-07 14:16:27 +0800

    p213 官方dp

--- 2023-4/p123.py
+++ 2023-4/p123.py
@@ -20,6 +20,16 @@ class Solution:
             r = max(r, left[i] + right[i])
         return r
 
+    def dp_plus(self, prices):
+        n = len(prices)
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
 
 
 print(Solution().maxProfit([1,2,4,2,5,7,2,4,9,0]))
